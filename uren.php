f8074a7df602a3b5add5d75b6a7946b9902579ca
markwage <mark.wage@hotmail.com>
2019-03-25 20:07:05 +0100

    Kleine aanpassingen aan de layout voor het invoeren van de uren

--- uren.php
+++ uren.php
@@ -17,8 +17,8 @@ include ("header.php");
 	
 <?php 
 displayUserGegevens(); 
-$currentWeek = date("W");
-$currentJaar = date("Y");
+//$currentWeek = date("W");
+//$currentJaar = date("Y");
 $inputweeknr=date('Y').date('W');
 getWeekdays($inputweeknr);
 
@@ -39,7 +39,8 @@ while($row_soorturen = mysqli_fetch_array($sql_soorturen)) {
 //------------------------------------------------------------------------------------------------------
 
 //------------------------------------------------------------------------------------------------------
-// BUTTON changeWeeknr
+// BUTTON changeWeeknr is op geklikt om een andere week te muteren.
+// Door getWeekdays worden de dagen en data van die nieuwe week berekend
 //------------------------------------------------------------------------------------------------------
 if (isset($_POST['updateweeknr'])) {
     $inputweeknr = $_POST["week"];
@@ -57,13 +58,20 @@ if (isset($_POST['cancel'])) {
 // BUTTON Save
 //------------------------------------------------------------------------------------------------------
 if (isset($_POST['save'])) {
-    $number = count($_POST["dag1"]);
-    if($number > 0) {
-        for($i=0; $i<$number; $i++) {
-            if(trim($_POST["soortuur"][$i] != '')) {
+    // Hoeveel invoerrijen op het scherm
+    $aantalRijen = count($_POST["dag1"]);
+    if($aantalRijen > 0) {
+        for($ix1=0; $ix1<$aantalRijen; $ix1++) {
+            if(trim($_POST["soortuur"][$ix1] != '')) {
                 //$sql = "INSERT INTO tbl_name(name) VALUES('".mysqli_real_escape_string($dbconn, $_POST["name"][$i])."') or die(mysqli_error($db))";
                 //mysqli_query($connect, $sql);
-                echo "Urensoort: ".$_POST["soortuur"][$i]."<br />";
+                
+                //001 Check de ingevulde velden op correctheid
+                //002 Check of de week al voorkomt in de database
+                //002A Zo ja dan eerst die week uit de database verwijderen
+                //003 Insert de waarden in de database
+                
+                echo "Urensoort: ".$_POST["soortuur"][$ix1]."<br />";
             }
         }
     } else {
@@ -77,7 +85,7 @@ if (isset($_POST['save'])) {
 	<table>
 		<tr>
 			<td>Weeknummer</td>
-			<td><input type="number" name="week" id="camp-week" value="<?php echo $inputweeknr; ?>" size="4" required></td>
+			<td><input style="width:70px" type="number" name="week" id="camp-week" value="<?php echo $inputweeknr; ?>" required></td>
 			<td><input class="button" type="submit" name="updateweeknr" value="refresh"></td>
 		</tr>
 	</table>
@@ -97,13 +105,13 @@ if (isset($_POST['save'])) {
    			<div id="dropdownSoortUren" data-options="<?php echo $option; ?>"></div>
    		    <!-- Het aantal td's moet in functions.js net zoveel zijn! -->
    		    <td><select name="soortuur[]"><?php echo $option; ?></select></td>
-    		<td><input type="number" name="dag1[]" min="0" max="24" step="0.25" size="2"></td>
-    		<td><input type="number" name="dag2[]" min="0" max="24" step="0.25" size="2"></td>
-    		<td><input type="number" name="dag3[]" min="0" max="24" step="0.25" size="2"></td>
-    		<td><input type="number" name="dag4[]" min="0" max="24" step="0.25" size="2"></td>
-    		<td><input type="number" name="dag5[]" min="0" max="24" step="0.25" size="2"></td>
-    		<td><input type="number" name="dag6[]" min="0" max="24" step="0.25" size="2"></td>
-    		<td><input type="number" name="dag7[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag1[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag2[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag3[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag4[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag5[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag6[]" min="0" max="24" step="0.25" size="2"></td>
+    		<td><input style="width:50px" type="number" name="dag7[]" min="0" max="24" step="0.25" size="2"></td>
     		<td><img src="./img/buttons/icons8-plus-48.png" alt="toevoegen soort uur" title="toevoegen soort uur" onclick="add_row();" /></td>
     		<td></td>
    		</tr>
