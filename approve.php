bdbf2252b551edfe83184a7bb2baaaf0bd622a50
mark <mark.wage@hotmail.com>
2020-03-05 13:25:42 +0100

    Gebruikt nu prepare-statement

--- approve.php
+++ approve.php
@@ -53,36 +53,33 @@ if (isset($_POST['afkeuren'])) {
 // BUTTON Approve
 // ------------------------------------------------------------------------------------------------------
 if (isset($_POST['approve'])) {
-    $approvedbyuser = $_SESSION['username'];
+    $approved_by_user = $_SESSION['username'];
     $username = $_POST['username'];
     $maand = $_POST['maand'];
     $jaar = $_POST['jaar'];
     $emailadres = $_POST['emailadres'];
-    $sql_code = "UPDATE uren
-                 SET approveddatum = '" . date('Y-m-d') . "',
-                 approvedbyuser = '" . $approvedbyuser . "',
-                 approved = '1'
-                 WHERE user = '" . $username . "'
-                 AND month(datum) = '" . $maand . "'
-                 AND year(datum) = '" . $jaar . "'";
-    $sql_out = mysqli_query($dbconn, $sql_code);
-    if (!$sql_out) {
-        writelog("approve", "ERROR", "Update uren gaat fout: " . $sql_code . " - " . mysqli_error($dbconn));
+    $approveddatum = date('Y-m-d');
+    $approved = 1;
+    
+    try {
+        $stmt_upd = $mysqli->prepare("UPDATE uren SET approveddatum = ?, approvedbyuser = ?, approved = ? WHERE user = ? AND MONTH(datum) = ? AND YEAR(datum) = ?");
+        $stmt_upd->bind_param("ssisii", $approveddatum, $approved_by_user, $approved, $username, $maand, $jaar);
+        $stmt_upd->execute();
+    } catch(Exception $e) {
+        writelog("approve", "ERROR", $e);
         exit($MSGDB001E);
     }
 
     writelog("approve", "INFO", "Voor user {$username} is maand {$jaar}-{$maand} approved");
-
-    $sql_insert = "INSERT INTO approvals (maand, jaar, user)
-                   VALUES('" . $maand . "', 
-                          '" . $jaar . "',
-                          '" . $username . "')";
-    $sql_out_insert = mysqli_query($dbconn, $sql_insert);
-    if (!$sql_out_insert) {
-        writelog("approve", "ERROR", "Insert van approval in tabel approvals gaat fout: " . $sql_code . " - " . mysqli_error($dbconn));
+    
+    try {
+        $stmt_ins = $mysqli->prepare("INSERT INTO approvals (maand, jaar, user) VALUES (?, ?, ?)");
+        $stmt_ins->bind_param("iis", $maand, $jaar, $username);
+        $stmt_ins->execute();
+    } catch(Exception $e) {
+        writelog("approve", "ERROR", $e);
         exit($MSGDB001E);
     }
-
     writelog("approve", "INFO", "Record succesvol toegevoegd in tabel approvals voor user {$username} periode {$jaar}-{$maand}");
 
     // Versturen mail naar user dat de maand approved is
@@ -96,36 +93,29 @@ if (isset($_POST['approve'])) {
 
     // Creeeren van de email message
     mail_message_header();
-    $message .= '<p>De uren van gebruiker ' . $username . ' betreffende maand <strong>' . $jaar . '-' . $maand . '</strong> zijn approved in Mirage Urenregistratie Systeem<br />Approved door: ' . $approvedbyuser . '</p>';
+    $message .= '<p>De uren van gebruiker ' . $username . ' betreffende maand <strong>' . $jaar . '-' . $maand . '</strong> zijn approved in Mirage Urenregistratie Systeem<br />Approved door: ' . $approved_by_user . '</p>';
 
     // Display totaal per soortuur
     $message .= '<p><strong>Totaal per urensoort</strong></p>';
     $message .= '<table>';
     $message .= '<tr><th colspan="2" style="text-align:left">Soort uur</th><th style="text-align:right">Totaal</th></tr>';
 
-    $sql_code = "SELECT sum(uren) AS toturen, soortuur, omschrijving, approved FROM view_uren_soortuur
-                WHERE user = '$username'
-                AND approval_maand = '$maand'
-                AND approval_jaar = '$jaar'
-                GROUP BY soortuur
-                ORDER BY soortuur";
-    $sql_out = mysqli_query($dbconn, $sql_code);
-    if (!$sql_out) {
-        writelog("approve", "ERROR", "Select van uren gaat fout bij het aanmaken van een mail: " . $sql_code . " - " . mysqli_error($dbconn));
+    try {
+        $stmt_sel_soort = $mysqli->prepare("SELECT sum(uren) AS toturen, soortuur, omschrijving FROM view_uren_soortuur WHERE user = ? AND approval_maand = ? AND approval_jaar = ? GROUP BY soortuur ORDER BY soortuur");
+        $stmt_sel_soort->bind_param("sii", $username, $maand, $jaar);
+        $stmt_sel_soort->execute();
+    } catch(Exception $e) {
+        writelog("beginsaldo", "ERROR", $e);
         exit($MSGDB001E);
     }
-    
-    while ($sql_rows = mysqli_fetch_array($sql_out)) {
-        $approved     = $sql_rows['approved'];
-        $totaal_uren  = $sql_rows['toturen'];
-        $soortuur     = $sql_rows['soortuur'];
-        $omschrijving = $sql_rows['omschrijving'];
-
+    $stmt_sel_soort->bind_result($totaal_uren, $soortuur, $omschrijving);
+    while($stmt_sel_soort->fetch()) {
         $message .= '<tr class="colored"><td>' . $soortuur . '</td><td>' . $omschrijving . '</td><td style=\'text-align:right\'><strong>' . $totaal_uren . '</strong></td></tr>';
     }
 
     $message .= '</table>';
     mail_message_footer($message);
+    writelog("approve", "INFO", $message);
     // Versturen van de email
     if ($_SERVER['SERVER_NAME'] != 'localhost') {
         if (mail($mail_to, $mail_subject, $message, $headers)) {
@@ -144,39 +134,34 @@ if (isset($_POST['approve'])) {
 // ******************* START *******************
 //
 // Dit wordt uitgevoerd wanneer de gebruiker in linkermenu op "Openstaande approvals" klikt
-// Er wordt een lijst met de te approven maanden getoond, max half jaar terug
-// Alleen die maanden worden getoond die niet approved zijn
+// Alles wordt van de laatste maand getoond behalve wat nog openstaat. Daarvan wordt alles getoond
 // ------------------------------------------------------------------------------------------------------
 if ($aktie == 'disp') {
-    ?> <form name="disp" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"> <?php 
-    $vorig_jaar_tot = date('Y', strtotime('-1 month', time()));
-    $vorige_maand_tot = date('m', strtotime('-1 month', time()));
-    $vorig_jaar_vanaf = date('Y', strtotime('-6 month', time()));
-    $vorige_maand_vanaf = date('m', strtotime('-6 month', time()));
-
-    $sql_code = "SELECT * FROM view_uren_get_full_username
-                WHERE uren_invullen = 1
-                GROUP BY username, approval_jaar, approval_maand
-                ORDER BY fullname, approval_jaar, approval_maand;";
-    $sql_out = mysqli_query($dbconn, $sql_code);
-    if (!$sql_out) {
-        writelog("approve", "ERROR", "Selecteren gaat fout: " . $sql_code . " - " . mysqli_error($dbconn));
-        exit($MSGDB001E);
-    }
-
+    ?> 
+    <form name="disp" action="<?php echo $_SERVER['PHP_SELF']; ?>" method="post"> 
+    <?php 
+    
     echo "<center><table>";
     echo "<tr><th>Medewerker</th><th>Maand</th><th></th></tr>";
+    
+    $vorig_jaar = date('Y', strtotime('-1 month', time()));
+    $vorige_maand = date('m', strtotime('-1 month', time()));
+    $huidige_maand = date('m');
 
-    while ($sql_rows = mysqli_fetch_array($sql_out)) {
-        $approved = $sql_rows['approved'];
-        $username = $sql_rows['username'];
+    try {
+        $stmt_maanden = $mysqli->prepare("SELECT approved, username, approval_maand, approval_jaar, fullname FROM view_uren_get_full_username WHERE uren_invullen = 1 GROUP BY username, approval_jaar, approval_maand ORDER BY fullname, approval_jaar, approval_maand");
+        $stmt_maanden->execute();
+    } catch(Exception $e) {
+        writelog("approve", "ERROR", $e);
+        exit($MSGDB001E);
+    }
+    
+    $stmt_maanden->bind_result($approved, $username, $maand, $jaar, $fullname);
+    while($stmt_maanden->fetch()) { 
         $encrypted_username = convert_string('encrypt', $username);
-        $maand = $sql_rows['approval_maand'];
-        $jaar = $sql_rows['approval_jaar'];
-        $fullname = $sql_rows['fullname'];
 
         // Maanden die nog niet approved zijn
-        if (($jaar != '') && ($approved == 0)) {
+        if (($jaar != '') && ($approved == 0) && ($maand != $huidige_maand)) {
             echo '<tr class="colored">
             <td style="height:1.2vw;"><b>' . $fullname . '</b></td><td style=\'text-align:center\'>' . $jaar . ' ' . $maand . '</td>
 			<td><a href="approve.php?aktie=dspuren&user=' . $encrypted_username . '&jaar=' . $jaar . '&maand=' . $maand . '"><img class="button" src="./img/icons/view-48.png" alt="Toon week" title="Toon/approve de uren van deze maand" /></a></td>
@@ -188,7 +173,7 @@ if ($aktie == 'disp') {
 			<td>Nog geen gegevens over afgelopen maand aanwezig</td>
 			</tr>';
         // Uren van afgelopen maand die approved zijn
-        } elseif (($approved == 1) && ($maand == $vorige_maand_tot) && ($jaar == $vorig_jaar_tot)) {
+        } elseif (($approved == 1) && ($maand == $vorige_maand) && ($jaar == $vorig_jaar)) {
             echo '<tr class="colored">
             <td style="height:1.2vw;"><b>' . $fullname . '</b></td><td style=\'text-align:center\'>' . $jaar . ' ' . $maand . '</td>
 			<td>Approved</td>
@@ -215,19 +200,17 @@ if ($aktie == 'dspuren') {
     $username = convert_string('decrypt',$_GET['user']);
     $maand = $_GET['maand'];
     $jaar = $_GET['jaar'];
-
-    $sql_code = "SELECT * FROM users
-                WHERE username = '$username'";
-    $sql_out = mysqli_query($dbconn, $sql_code);
-    if (!$sql_out) {
-        writelog("approve", "ERROR", "Selecteen van users gaat fout: " . $sql_code . " - " . mysqli_error($dbconn));
+    try {
+        $stmt_user = $mysqli->prepare("SELECT voornaam, tussenvoegsel, achternaam, emailadres FROM users WHERE username = ?");
+        $stmt_user->bind_param("s", $username);
+        $stmt_user->execute();
+        $stmt_user->store_result();
+    } catch(Exception $e) {
+        writelog("approve", "ERROR", $e);
         exit($MSGDB001E);
     }
-    $sql_rows = mysqli_fetch_array($sql_out);
-    $voornaam = $sql_rows['voornaam'];
-    $tussenvoegsel = $sql_rows['tussenvoegsel'];
-    $achternaam = $sql_rows['achternaam'];
-    $emailadres = $sql_rows['emailadres'];
+    $stmt_user->bind_result($voornaam, $tussenvoegsel, $achternaam, $emailadres);
+    $stmt_user->fetch();
 
     echo "<center><b>Maand: </b>" . $jaar . " " . $maand . "<br /><b>Medewerker: </b>" . $voornaam . " " . $tussenvoegsel . " " . $achternaam . " ";
     echo "<h3>Overzicht per dag</h3>";
@@ -242,17 +225,17 @@ if ($aktie == 'dspuren') {
 
     // Creeeren van het SQL statement.
     // De variabele wordt in meerdere regels uitgebreid middels .=
+    // Dit is nodig om voor elke dag een SUM(CASE te creeeren. De laatste dag is apart omdat deze niet met een komma moet eindigen 
     $sql_code = "SELECT *,";
     for($ix=1; $ix<31; $ix++) {
         $sql_code .= "SUM(CASE WHEN approval_dag = '".$ix."' THEN uren END) AS `".$ix."`,";
     }
-    $sql_code .= "SUM(CASE WHEN approval_dag = '31' THEN uren END) AS `31`
-        FROM view_uren_soortuur
-            WHERE USER = '".$username."'
-            AND approval_maand = '".$maand."'
-            AND approval_jaar = '".$jaar."'
-            GROUP BY soortuur
-            ORDER BY soortuur, datum"; 
+    
+    $sql_code .= "SUM(CASE WHEN approval_dag = '31' THEN uren END) AS `31` 
+                  FROM view_uren_soortuur
+                  WHERE USER = '".$username."' AND approval_maand = '".$maand."' AND approval_jaar = '".$jaar."'
+                  GROUP BY soortuur 
+                  ORDER BY soortuur, datum"; 
     
     $sql_out = mysqli_query($dbconn, $sql_code);
     if (!$sql_out) {
@@ -278,7 +261,7 @@ if ($aktie == 'dspuren') {
     echo "<h3>Totaal per urensoort</h3>";
     echo "<center><table>";
     echo "<tr><th>Soort uur</th><th>Totaal</th></tr>";
-
+    /**
     $sql_code = "SELECT SUM(uren) AS totaal_uren, soortuur, omschrijving, approved FROM view_uren_soortuur
                 WHERE user = '$username'
                 AND approval_maand = '$maand'
@@ -292,16 +275,27 @@ if ($aktie == 'dspuren') {
     }
     
     while ($sql_rows = mysqli_fetch_array($sql_out)) {
-        $approved     = $sql_rows['approved'];
-        $totaal_uren  = $sql_rows['totaal_uren'];
-        $soortuur     = $sql_rows['soortuur'];
-        $omschrijving = $sql_rows['omschrijving'];
-
+    */  
+    //$driver = new mysqli_driver();
+    //$driver->report_mode = MYSQLI_REPORT_ALL;
+    try {
+        $stmt_uren = $mysqli->prepare("SELECT SUM(uren) AS totaal_uren, soortuur, omschrijving, approved FROM view_uren_soortuur WHERE user = ? AND approval_maand = ? AND approval_jaar = ? GROUP BY soortuur ORDER BY soortuur");
+        $stmt_uren->bind_param("sii", $username, $maand, $jaar);
+        $stmt_uren->execute();
+    } catch(Exception $e) {
+        writelog("approve", "ERROR", $e);
+        exit($MSGDB001E);
+    }
+    $stmt_uren->bind_result($totaal_uren, $soortuur, $omschrijving, $approved);
+    while($stmt_uren->fetch()) {
+        //$approved     = $sql_rows['approved'];
+        //$totaal_uren  = $sql_rows['totaal_uren'];
+        //$soortuur     = $sql_rows['soortuur'];
+        //$omschrijving = $sql_rows['omschrijving'];
         echo '<tr class="colored">
 			<td>' . $soortuur . ' - ' . $omschrijving . '</td><td style=\'text-align:right\'>' . $totaal_uren . '</td>
             </tr>';
     }
-
     echo "</table></center>";
 
     // Display buttons. Met behulp van een formulier
